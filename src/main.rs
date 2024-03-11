250e8c91c3da0c860cdcca225665a83e6eb875d2
asempere123123 <asempere158@gmail.com>
2024-03-11 17:28:39 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use yew_router::prelude::*;
+use yew::prelude::*;
+
+mod pages;
+
+use pages::Home;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! { <Home /> },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
+
+#[function_component(Main)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
+fn main() {
+    yew::Renderer::<Main>::new().render();
+}
