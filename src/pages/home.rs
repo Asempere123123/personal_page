c96ea94e535333af161919f7b2e3a41bfccede30
Asempere123123 <asempere@colegioaleman.net>
2024-03-11 17:56:15 +0100

    first commit

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -2,8 +2,14 @@ use yew::prelude::*;
 
 #[function_component(Home)]
 pub fn home() -> Html {
-
     html! {
-        <h1>{" hola "}</h1>
+        <div class="container mt-16 mx-48 grid grid-cols-3">
+            <div class="col-span-1">
+                <h1 class="">{" hola "}</h1>
+            </div>
+            <div class="col-span-2">
+                <h1 class="">{" hola "}</h1>
+            </div>
+        </div>
     }
 }
